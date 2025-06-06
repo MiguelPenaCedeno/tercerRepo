9dac11dff45603bee6624012f93129288f5dbd73
Miguel Peña <miguelangelpenacedeno@gmail.com>
2025-06-06 10:43:55 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Miguel Peña",                         # Tu nombre
+    author_email="miguelangelpenacedeno@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/MiguelPenaCedeno/tercerrepo",     # URL del proyecto
+)
